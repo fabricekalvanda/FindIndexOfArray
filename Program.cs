cbf8914ccaa272cc48c32f26646934fd108274e2
FABRICE KALVANDA <fabricekalvanda@gmail.com>
2021-02-28 18:09:06 -0800

    balance method implimented

--- Program.cs
+++ Program.cs
@@ -4,14 +4,36 @@ namespace FindIndexOfArray
 {
     class Program
     {
+        static int Balance(int[] arrayy, int x)
+        {
+            int i, j;
+            int leftSum, rightSum;
+
+
+            for (i = 0; i < x; ++i)
+            {
+                leftSum = 0;
+
+                rightSum = 0;
+
+                for (j = 0; j < i; j++)
+                    leftSum += arrayy[j];
+
+                for (j = i + 1; j < x; j++)
+                    rightSum += arrayy[j];
+
+                if (leftSum == rightSum)
+                    return i;
+            }
+
+            return -1;
+        }
         static void Main(string[] args)
         {
-            int[] arrayy = { -8, 2, 6, 2, -5, 4, 1 }; 
+            int[] arrayy = { 2, 3, 5, 5, 2 };
+            int arrayBalance = arrayy.Length;
 
-            for (int i = 0; i < arrayy.Length; i++)
-            {
-                Console.Write(arrayy[i] + " ");
-            }
+            Console.Write(Balance(arrayy, arrayBalance));
         }
     }
 }
